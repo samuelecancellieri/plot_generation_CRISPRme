8ae591374ab7fdd2be3c21bda3bf025837d80c29
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-11-19 11:09:21 +0100

    update import

--- gene_annotation_extraction_plots.py
+++ gene_annotation_extraction_plots.py
@@ -1,7 +1,9 @@
 # from adjustText import adjust_text
 import warnings
+import matplotlib
+import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from matplotlib.image import BboxImage
+# from matplotlib.image import BboxImage
 import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
 import sys
@@ -9,8 +11,7 @@ import os
 import pandas as pd
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-import matplotlib
+
 # set matplotlib to not use X11 server
 matplotlib.use('Agg')
 
