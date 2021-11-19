e43fbfcf6fde0869ad653b5c321de86bc3d51eb7
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-11-19 11:50:20 +0100

    update normalized values

--- gene_annotation_extraction_plots.py
+++ gene_annotation_extraction_plots.py
@@ -99,15 +99,14 @@ def crisprme_plot_MMvBUL(df, guide, out_folder, max_mm_bul_value, pam_first_nucl
                                                                           'Mismatches+bulges_(fewest_mm+b)']
         df.loc[index, 'Mismatches+bulges_(fewest_mm+b)_DELTA'] = int(df.loc[index,
                                                                             'Mismatches+bulges_(fewest_mm+b)_REF']) - int(df.loc[index, 'Mismatches+bulges_(fewest_mm+b)'])
-        
-    max_mm_bul_ref_norm = df['Mismatches+bulges_(fewest_mm+b)_REF'].max()
-    max_mm_bul_alt_norm = df['Mismatches+bulges_(fewest_mm+b)'].max()
 
     df['Mismatches+bulges_(fewest_mm+b)_REF_NORM'] = df['Mismatches+bulges_(fewest_mm+b)_REF'] / \
-        max_mm_bul_ref_norm
+        df['Mismatches+bulges_(fewest_mm+b)'].max()
+    df.loc[df['Mismatches+bulges_(fewest_mm+b)_REF_NORM']
+           > 1, 'Mismatches+bulges_(fewest_mm+b)_REF_NORM'] = 1
     df['Mismatches+bulges_(fewest_mm+b)_ALT_NORM'] = df['Mismatches+bulges_(fewest_mm+b)'] / \
-        max_mm_bul_alt_norm
-        
+        df['Mismatches+bulges_(fewest_mm+b)'].max()
+
     # sort the df
     df.sort_values(['Mismatches+bulges_(fewest_mm+b)', 'Mismatches+bulges_(fewest_mm+b)_DELTA', 'Variant_MAF_(fewest_mm+b)'],
                    inplace=True, ascending=True)
