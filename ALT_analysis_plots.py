01316d9935bdd4da5cfd06b4a939376c2446790f
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-11-26 15:18:56 +0100

    ll

--- ALT_analysis_plots.py
+++ ALT_analysis_plots.py
@@ -189,7 +189,7 @@ def generate_heatmap_CFD(original_df):
 
     figu = plt.figure()
     plt_heatmap = sns.heatmap(table, annot=True, norm=LogNorm())
-    plt_heatmap.collections[0].colorbar.ax.yaxis.set_ticks([], minor=True)
+    plt_heatmap.collections[0].colorbar.ax.yaxis.set_ticks([], minor=False)
     # plt_heatmap.set_xticklabels(
     #     labels=df_heatmap['Variant_MAF_(highest_CFD)'].unique(), ha="left")
     # plt_heatmap.set_yticklabels(
