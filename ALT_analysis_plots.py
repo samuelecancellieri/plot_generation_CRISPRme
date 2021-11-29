e26b09c5ad96d419bb45fcbf331dcff53db23662
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-11-29 16:39:23 +0100

    check af type

--- ALT_analysis_plots.py
+++ ALT_analysis_plots.py
@@ -243,7 +243,8 @@ def generate_distribution_plot_CFD(original_df):
         altTarget_MAF05 = 0
         altTarget_MAF0 = 0
 
-        for af in guide_df['AF']:
+        for af in guide_df['AF'].tolist():
+            print(type(af))
             if af > 0.005:
                 altTarget_MAF005 += 1
             if af > 0.05:
