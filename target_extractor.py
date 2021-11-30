9583ae8653e60bbeffee1dfbe690b0d5746539e8
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-11-30 14:28:32 +0100

    print data per guide

--- target_extractor.py
+++ target_extractor.py
@@ -6,23 +6,25 @@ original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
                           na_values=['n'])
 
 
-alt_target_count_CFD = original_df.apply(lambda x: True
-                                         if x['REF/ALT_origin_(highest_CFD)'] == "alt" else False, axis=1)
-alt_target_count_MMvBUL = original_df.apply(lambda x: True
-                                            if x['REF/ALT_origin_(fewest_mm+b)'] == "alt" else False, axis=1)
+for guide in original_df['Spacer+PAM'].unique():
 
-# Count number of True in the series
-original_df['alt_target_cfd'] = len(
-    alt_target_count_CFD[alt_target_count_CFD == True].index)
+    alt_target_count_CFD = original_df.apply(lambda x: True
+                                             if x['REF/ALT_origin_(highest_CFD)'] == "alt" else False, axis=1)
+    alt_target_count_MMvBUL = original_df.apply(lambda x: True
+                                                if x['REF/ALT_origin_(fewest_mm+b)'] == "alt" else False, axis=1)
 
-original_df['alt_target_mmvbul'] = len(
-    alt_target_count_MMvBUL[alt_target_count_MMvBUL == True].index)
+    # Count number of True in the series
+    original_df['alt_target_cfd'] = len(
+        alt_target_count_CFD[alt_target_count_CFD == True].index)
 
-original_df['total_target'] = len(original_df.index)
+    original_df['alt_target_mmvbul'] = len(
+        alt_target_count_MMvBUL[alt_target_count_MMvBUL == True].index)
 
-on_target_df = original_df.loc[(
-    original_df['Mismatches+bulges_(highest_CFD)'] <= 1)]
+    original_df['total_target'] = len(original_df.index)
 
-print(on_target_df[['Spacer+PAM', 'Chromosome',
-                    'Start_coordinate_(highest_CFD)', 'Aligned_protospacer+PAM_REF_(highest_CFD)',
-                    'Annotation_closest_gene_name', 'total_target', 'alt_target_cfd', 'alt_target_mmvbul']])
+    on_target_df = original_df.loc[(
+        original_df['Mismatches+bulges_(highest_CFD)'] <= 1)]
+
+    print(on_target_df[['Spacer+PAM', 'Chromosome',
+                        'Start_coordinate_(highest_CFD)', 'Aligned_protospacer+PAM_REF_(highest_CFD)',
+                        'Annotation_closest_gene_name', 'total_target', 'alt_target_cfd', 'alt_target_mmvbul']])
