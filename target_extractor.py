5a0d00f1fb53b96fcd67992161b742b9fac31f49
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-11-30 14:27:41 +0100

    count targets and on-target data

--- target_extractor.py
+++ target_extractor.py
@@ -12,14 +12,17 @@ alt_target_count_MMvBUL = original_df.apply(lambda x: True
                                             if x['REF/ALT_origin_(fewest_mm+b)'] == "alt" else False, axis=1)
 
 # Count number of True in the series
-alt_target_count_CFD = len(
+original_df['alt_target_cfd'] = len(
     alt_target_count_CFD[alt_target_count_CFD == True].index)
 
-alt_target_count_MMvBUL = len(
+original_df['alt_target_mmvbul'] = len(
     alt_target_count_MMvBUL[alt_target_count_MMvBUL == True].index)
+
+original_df['total_target'] = len(original_df.index)
 
 on_target_df = original_df.loc[(
     original_df['Mismatches+bulges_(highest_CFD)'] <= 1)]
 
-print(on_target_df[['Spacer+PAM', 'Chromosome', 'Start_coordinate_(highest_CFD)', 'Aligned_protospacer+PAM_REF_(highest_CFD)', 'Annotation_closest_gene_name']], len(original_df.index),
-      alt_target_count_CFD, alt_target_count_MMvBUL)
+print(on_target_df[['Spacer+PAM', 'Chromosome',
+                    'Start_coordinate_(highest_CFD)', 'Aligned_protospacer+PAM_REF_(highest_CFD)',
+                    'Annotation_closest_gene_name', 'total_target', 'alt_target_cfd', 'alt_target_mmvbul']])
