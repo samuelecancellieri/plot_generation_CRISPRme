684be2b606b525c16556613b8032cfaf39a1ede4
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-12-10 17:41:04 +0100

    heatmap

--- ALT_analysis_plots.py
+++ ALT_analysis_plots.py
@@ -191,7 +191,7 @@ def generate_heatmap_CFD(original_df):
 
     df_table = df_heatmap.groupby(
         ["Variant_MAF_(highest_CFD)", "CFD_score_(highest_CFD)"]).size().reset_index(name="Value")
-    df_table = df_table.apply(pd.to_numeric, downcast="int")
+    df_table = df_table.apply(pd.to_numeric)
 
     table = df_table.pivot('CFD_score_(highest_CFD)',
                            'Variant_MAF_(highest_CFD)', 'Value').fillna(0)
