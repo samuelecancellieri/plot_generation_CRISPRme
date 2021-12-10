f8392f30fedaf20be9440409eec5b3a923c1a6a3
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-12-10 11:02:00 +0100

    test non null

--- ALT_analysis_plots.py
+++ ALT_analysis_plots.py
@@ -330,9 +330,9 @@ original_df_read = pd.read_csv(inTargets, sep="\t", index_col=False,
                                na_values=['n'])
 
 print('pam creation')
-print(original_df_read[['PAM_creation_(highest_CFD)']].where(
-    ['PAM_creation_(highest_CFD)'].notnull()))
+notnull_df = original_df_read['PAM_creation_(highest_CFD)'].notnull()
 
+print(notnull_df[['PAM_creation_(highest_CFD)']])
 # call to plot generation CFD with original data
 # generate_distribution_plot_CFD(original_df_read, 'cfd_01')
 # generate_distribution_plot_CFD(original_df_read, 'no_filter')
