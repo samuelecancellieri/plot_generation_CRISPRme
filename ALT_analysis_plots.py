dc87c4ba83bf2fc88c048dac3aecf98fbba59eaf
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-12-13 15:44:21 +0100

    update fake targets

--- ALT_analysis_plots.py
+++ ALT_analysis_plots.py
@@ -180,8 +180,8 @@ def generate_heatmap_CFD(original_df):
     df_heatmap["Variant_MAF_(highest_CFD)"] = df_heatmap["Variant_MAF_(highest_CFD)"].apply(
         lambda x: num_of_decimal_zeros(x))
 
-    df_fake_target = pd.DataFrame([1, 0.00001], [1, 0.0001], [1, 0.001], [1, 0.01], [
-                                  1, 0.1], columns=['CFD_score_(highest_CFD)', 'Variant_MAF_(highest_CFD)'])
+    df_fake_target = pd.DataFrame([[1, 0.00001], [1, 0.0001], [1, 0.001], [1, 0.01], [
+                                  1, 0.1]], columns=['CFD_score_(highest_CFD)', 'Variant_MAF_(highest_CFD)'])
     df_heatmap.append(df_fake_target)
 
     # CFD score rounding to 1 decimal
