77e237f693d685a4715a3f5b4351c00cf77ddd5c
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-07 13:44:49 +0100

    fix typo to extract columns

--- excel_writer.py
+++ excel_writer.py
@@ -10,25 +10,25 @@ writer = pd.ExcelWriter(sys.argv[2]+'guide_sheets.xlsx')
 for guide in original_df['Spacer+PAM'].unique():
     guide_df = original_df.loc[(original_df['Spacer+PAM'] == guide)]
 
-    guide_df = guide_df[guide_df[['Spacer+PAM', 'Chromosome',
-                                  'Start_coordinate_(highest_CFD)',
-                                  'Aligned_protospacer+PAM_REF_(highest_CFD)',
-                                  'Aligned_protospacer+PAM_ALT_(highest_CFD)',
-                                  'Mismatches+bulges_(highest_CFD)',
-                                  'CFD_score_(highest_CFD)',
-                                  'Start_coordinate_(fewest_mm+b)',
-                                  'Aligned_protospacer+PAM_REF_(fewest_mm+b)',
-                                  'Aligned_protospacer+PAM_ALT_(fewest_mm+b)',
-                                  'Mismatches+bulges_(fewest_mm+b)',
-                                  'CFD_score_(fewest_mm+b)',
-                                  'Start_coordinate_(highest_CRISTA)',
-                                  'Aligned_protospacer+PAM_REF_(highest_CRISTA)',
-                                  'Aligned_protospacer+PAM_ALT_(highest_CRISTA)',
-                                  'Mismatches+bulges_(highest_CRISTA)',
-                                  'CRISTA_score_(highest_CRISTA)',
-                                  'Annotation_GENCODE',
-                                  'Annotation_closest_gene_name',
-                                  'Annotation_ENCODE']]]
+    guide_df = guide_df[['Spacer+PAM', 'Chromosome',
+                         'Start_coordinate_(highest_CFD)',
+                         'Aligned_protospacer+PAM_REF_(highest_CFD)',
+                         'Aligned_protospacer+PAM_ALT_(highest_CFD)',
+                         'Mismatches+bulges_(highest_CFD)',
+                         'CFD_score_(highest_CFD)',
+                         'Start_coordinate_(fewest_mm+b)',
+                         'Aligned_protospacer+PAM_REF_(fewest_mm+b)',
+                         'Aligned_protospacer+PAM_ALT_(fewest_mm+b)',
+                         'Mismatches+bulges_(fewest_mm+b)',
+                         'CFD_score_(fewest_mm+b)',
+                         'Start_coordinate_(highest_CRISTA)',
+                         'Aligned_protospacer+PAM_REF_(highest_CRISTA)',
+                         'Aligned_protospacer+PAM_ALT_(highest_CRISTA)',
+                         'Mismatches+bulges_(highest_CRISTA)',
+                         'CRISTA_score_(highest_CRISTA)',
+                         'Annotation_GENCODE',
+                         'Annotation_closest_gene_name',
+                         'Annotation_ENCODE']]
 
     # extract top 1000 rows for each guide
     guide_df = guide_df.head(1000)
