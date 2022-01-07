2cd60f7d3f811d936b0823e2aa5daf1a69dbef22
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-07 14:32:26 +0100

    update to keep na and include risk score

--- excel_writer.py
+++ excel_writer.py
@@ -16,16 +16,19 @@ for guide in original_df['Spacer+PAM'].unique():
                          'Aligned_protospacer+PAM_ALT_(highest_CFD)',
                          'Mismatches+bulges_(highest_CFD)',
                          'CFD_score_(highest_CFD)',
+                         'CFD_risk_score_(highest_CFD)',
                          'Start_coordinate_(fewest_mm+b)',
                          'Aligned_protospacer+PAM_REF_(fewest_mm+b)',
                          'Aligned_protospacer+PAM_ALT_(fewest_mm+b)',
                          'Mismatches+bulges_(fewest_mm+b)',
                          'CFD_score_(fewest_mm+b)',
+                         'CFD_risk_score_(highest_CFD)',
                          'Start_coordinate_(highest_CRISTA)',
                          'Aligned_protospacer+PAM_REF_(highest_CRISTA)',
                          'Aligned_protospacer+PAM_ALT_(highest_CRISTA)',
                          'Mismatches+bulges_(highest_CRISTA)',
                          'CRISTA_score_(highest_CRISTA)',
+                         'CRISTA_risk_score_(highest_CRISTA)',
                          'Annotation_GENCODE',
                          'Annotation_closest_gene_name',
                          'Annotation_ENCODE']]
@@ -34,7 +37,7 @@ for guide in original_df['Spacer+PAM'].unique():
     guide_df = guide_df.head(1000)
 
     # generate excel sheets
-    guide_df.to_excel(writer, sheet_name=str(guide))
+    guide_df.to_excel(writer, sheet_name=str(guide), na_rep='NA')
 
 # save the writer to excel file
 writer.save()
