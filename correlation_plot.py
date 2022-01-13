1bd13dcea9dd05dfbd2f925c1b46bab98e8a3f6f
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-13 17:48:05 +0100

    plot correlation with diffusion plot seaborn

--- correlation_plot.py
+++ correlation_plot.py
@@ -14,13 +14,16 @@ matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
 
-def plot_correlation(nrows, original_df):
-    original_df_filtered = original_df.head(nrows)
+def plot_correlation(original_df):
+    original_df_filtered = original_df
 
     plt.figure()
 
-    ax = sns.regplot(data=original_df_filtered, x="CFD_score_(highest_CFD)",
-                     y="CRISTA_score_(highest_CRISTA)", fit_reg=True, marker="+", color="skyblue")
+    ax = sns.displot(
+        data=original_df_filtered, x="CFD_score_(highest_CFD)", y="CRISTA_score_(highest_CRISTA)", kind="kde", rug=True, color='orange')
+
+    # sns.regplot(data=original_df_filtered, x="CFD_score_(highest_CFD)",
+    #             y="CRISTA_score_(highest_CRISTA)", fit_reg=True, marker="+", color="skyblue")
 
     # ax = sns.scatterplot(x="CFD_score_(highest_CFD)",
     #                      y="CRISTA_score_(highest_CRISTA)", data=original_df_filtered)
@@ -28,21 +31,21 @@ def plot_correlation(nrows, original_df):
     #            y="CRISTA_score_(highest_CRISTA)", data=original_df_filtered)
     ax.set(xlabel='CFD Score', ylabel='CRISTA Score')
 
-    ax.set_title("Score correlation CFD vs CRISTA top"+str(nrows)+' rows')
+    ax.set_title("Score correlation CFD vs CRISTA top")
 
     print(stats.pearsonr(original_df_filtered['CFD_score_(highest_CFD)'],
                          original_df_filtered['CRISTA_score_(highest_CRISTA)']))
 
     plt.tight_layout()
-    plt.savefig(sys.argv[2]+'correlation_CFDvCRISTA_top'+str(nrows)+'.pdf')
+    plt.savefig(sys.argv[2]+'correlation_CFDvCRISTA_top.pdf')
     plt.clf()
     plt.close('all')
 
 
 print('start processing')
 original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
-                          na_values=['n'], nrows=1000)
+                          na_values=['n'])
 # correlation with top1000 rows
-plot_correlation(1000, original_df)
+plot_correlation(original_df)
 # correlation with top100 rows
-plot_correlation(100, original_df)
+# plot_correlation(100, original_df)
