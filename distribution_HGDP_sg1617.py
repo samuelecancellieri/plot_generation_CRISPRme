09e39670aea70d0fa9f4717d2416e9f66020a353
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-14 14:08:24 +0100

    test plot with CI and pops

--- distribution_HGDP_sg1617.py
+++ distribution_HGDP_sg1617.py
@@ -1,5 +1,26 @@
 import sys
+import time
+import random
 import pandas as pd
+from pandas.core.indexes.api import all_indexes_same
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+# from upsetplot import generate_counts
+from upsetplot import UpSet
+# import matplotlib as mpl
+from upsetplot import from_memberships
+import warnings
+import matplotlib
+import math
+# SUPPRESS ALL WARNINGS
+warnings.filterwarnings("ignore")
+# do not use X11
+matplotlib.use('Agg')
+# set matplotlib for pdf editing
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
 # original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
 #                           na_values=['n'])
@@ -27,6 +48,48 @@ for line in sample_file:
 #                           >= 0.2 & sg1617_df['CFD_risk_score_(highest_CFD)'] >= 0.1)]
 # sg1617_df = sg1617_df.loc[(sg1617_df['Variant_samples_(highest_CFD)'].str.contains(
 #     'HGDP') & ~sg1617_df['Variant_samples_(highest_CFD)'].str.contains('NA'))]
+
+
+def printDensityPlot():
+    # create figure and set axis
+    plt.figure()
+    for pop in sample_dict:  # for each superpopulation
+        andamenti = list()
+        permutationList = list()
+        for sample in sample_dict[pop]:
+            # append samples to list to permute
+            permutationList.append(sample)
+        print('DOING SUPERPOPULATION PLOT FOR: ', pop)
+        for permutation in range(0, 100):
+            np.random.shuffle(permutationList)
+            andamento = list()
+            alreadyAddedTargets = set()
+            for sample in permutationList:
+                alreadyAddedTargets = alreadyAddedTargets.union(
+                    sample_dict[pop][sample])
+                andamento.append(len(alreadyAddedTargets))
+            andamenti.append(andamento)
+        # read values to generate plot
+        andamentiArray = np.array(andamenti)
+        media = np.mean(andamentiArray, axis=0)
+        standarddev = np.std(andamentiArray, axis=0)
+        standarderr = standarddev/np.sqrt(len(list(sample_dict[pop])))
+        z_score = 1.96  # for confidence 95%
+        lowerbound = media-(z_score*standarderr)
+        upperbound = media+(z_score*standarderr)
+        # allMedie.append(media)
+        plt.plot(media, label=str(pop))
+        plt.fill_between(range(len(media)), lowerbound,
+                         upperbound, alpha=0.10)
+
+    plt.title('populations_with diffCFD >=' + str(0.1) +
+              ' and CI '+str(95)+'%'+' and CFD score >='+str(0.2))
+    plt.xlabel('# Individuals')
+    plt.ylabel('# Cumulative Targets')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(sys.argv[3]+'_allpop_with_diffCFD_'+str(0.1) +
+                'and_CI_95_and_CFD_score_'+str(0.2)+'.pdf')
 
 
 for index, target in enumerate(target_file):
