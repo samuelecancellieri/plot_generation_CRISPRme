b957d5d1a516c36bfba04c01c0f52300e93d0685
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-19 10:01:51 +0100

    correlation plots with scatter, union and venn

--- correlation_plot.py
+++ correlation_plot.py
@@ -15,46 +15,30 @@ matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
 
-def plot_correlation(original_df_filtered):
+def plot_correlation(guide, original_df_filtered):
+
     print('plotting')
     original_df_cfd_sort = original_df_filtered.sort_values(
         ['CFD_score_(highest_CFD)'], ascending=False)
     original_df_crista_sort = original_df_filtered.sort_values(
         ['CRISTA_score_(highest_CRISTA)'], ascending=False)
 
-    print('cfd order')
-    print(original_df_cfd_sort.head(
-        100)[['CFD_score_(highest_CFD)', 'CRISTA_score_(highest_CRISTA)']])
-
-    print('crista order')
-    print(original_df_crista_sort.head(100)[[
-          'CFD_score_(highest_CFD)', 'CRISTA_score_(highest_CRISTA)']])
-
-    df_union_crista_cfd = pd.concat(
+    # union of top100 CFD & CRISTA
+    df_union_crista_cfd_100 = pd.concat(
         [original_df_crista_sort.head(100), original_df_cfd_sort.head(100)]).drop_duplicates()
-    print('union of cfd and crista', len(df_union_crista_cfd.index))
 
     plt.figure()
 
     ax = sns.displot(
         data=original_df_filtered, x="CFD_score_(highest_CFD)", y="CRISTA_score_(highest_CRISTA)", kind="kde", color='orange')
 
-    sns.regplot(data=df_union_crista_cfd, x="CFD_score_(highest_CFD)",
+    sns.regplot(data=df_union_crista_cfd_100, x="CFD_score_(highest_CFD)",
                 y="CRISTA_score_(highest_CRISTA)", fit_reg=True, marker="+", color="skyblue")
 
-    # sns.scatterplot(x="CFD_score_(highest_CFD)",
-    #                 y="CRISTA_score_(highest_CRISTA)", data=df_union_crista_cfd, marker='+', color="skyblue")
-    # sns.lmplot(x="CFD_score_(highest_CFD)",
-    #            y="CRISTA_score_(highest_CRISTA)", data=original_df_filtered)
     ax.set(xlabel='CFD Score', ylabel='CRISTA Score')
 
-    # ax.set_title("Score correlation CFD vs CRISTA top")
-
-    print(stats.pearsonr(original_df_filtered['CFD_score_(highest_CFD)'],
-                         original_df_filtered['CRISTA_score_(highest_CRISTA)']))
-
     plt.tight_layout()
-    plt.savefig(sys.argv[2]+'correlation_CFDvCRISTA_top.pdf')
+    plt.savefig(sys.argv[2]+f'correlation_CFDvCRISTA_{guide}_top100_union.pdf')
     plt.clf()
     plt.close('all')
 
@@ -63,18 +47,32 @@ def plot_correlation(original_df_filtered):
     set_cfd = set(original_df_cfd_sort.head(100).index)
     set_crista = set(original_df_crista_sort.head(100).index)
     venn2([set_cfd, set_crista], ('CFD', 'CRISTA'))
-    plt.savefig(sys.argv[2]+'venn_CFDvCRISTA_top.pdf')
+
+    plt.tight_layout()
+    plt.savefig(sys.argv[2]+f'venn_CFDvCRISTA_{guide}_top100_union.pdf')
+    plt.clf()
+    plt.close('all')
+
+    plt.figure()
+
+    sns.scatterplot(x=list(original_df_cfd_sort.head(100).index), y=list(
+        original_df_crista_sort.head(100).index), marker='+', color="skyblue")
+
+    plt.tight_layout()
+    plt.savefig(sys.argv[2]+f'scatter_rank_CFDvCRISTA_{guide}_top100.pdf')
     plt.clf()
     plt.close('all')
 
 
 print('start processing')
 original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
-                          na_values=['n'], nrows=100000)
+                          na_values=['n'])
 
 # filter df to remove on-targets and mutant on-targets
 original_df = original_df.loc[(
     original_df['Mismatches+bulges_(highest_CFD)'] > 1)]
 
 # correlation plot exec
-plot_correlation(original_df)
+for guide in original_df["Spacer+PAM"].unique():
+    df_guide = original_df.loc[(original_df['Spacer+PAM'] == guide)]
+    plot_correlation(guide, df_guide)
