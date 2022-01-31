6b72a802b92f473c18ad7a7447d503c2194c5afe
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-31 15:19:02 +0100

    test paper size in plot

--- gene_annotation_extraction_plots.py
+++ gene_annotation_extraction_plots.py
@@ -22,7 +22,7 @@ matplotlib.use('Agg')
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 # plt.style.use('seaborn-poster')
-sns.set_context("poster")
+sns.set_context("paper")
 
 
 iupac_code_set = {
