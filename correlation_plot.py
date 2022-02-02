ab68539ff21fe08a163d9a3e8904b6442f83f7ad
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-02 10:53:21 +0100

    update rank

--- correlation_plot.py
+++ correlation_plot.py
@@ -113,6 +113,8 @@ def plot_correlation(original_df):
         # sort values in the union by CFD, then extract the index list ordered to use as x coordinates
         top1000_union_CFDvCRISTA.sort_values(
             ['CFD_score_(highest_CFD)'], ascending=False, inplace=True)
+        # reset index to use as x position for CFD rank
+        df_guide.reset_index(inplace=True)
         sorted_cfd_index_list = list(top1000_union_CFDvCRISTA['index'])
         cfd_score_list = list(
             top1000_union_CFDvCRISTA['CFD_score_(highest_CFD)'])
@@ -145,8 +147,8 @@ def plot_correlation(original_df):
                 crista_rank = str(sorted_crista_index_list.index(rank)+1)
             except:
                 crista_rank = 'out_of_list'
-            save = str(guide)+'\t' + str(cfd_score_list[pos])+'\t'+str(crista_score_list[pos])+'\t'+str(
-                rank+1)+'\t'+crista_rank+'\n'
+            save = str(guide)+'\t' + str(cfd_score_list[pos])+'\t'+str(
+                crista_score_list[pos])+'\t'+str(rank+1)+'\t'+crista_rank+'\n'
             union_file.write(save)
 
         # extend the list for plotting the whole distribution
