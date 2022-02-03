2fab69f6dfdd29299f01b7b329e29928c586f122
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-03 13:09:43 +0100

    update title

--- gene_annotation_extraction_plots.py
+++ gene_annotation_extraction_plots.py
@@ -607,8 +607,8 @@ def plot_title_figure(guide: str, mm: int, bul: int, cas_protein: str, genome: s
     gene_target = gene_target_dict[guide]
 
     fig = plt.figure(figsize=(8.5, 0.2))
-    fig.suptitle(guide+' '+gene_target+' '+cas_protein+' '+genome +
-                 ' '+str(mm)+' mismatches'+' + '+str(bul)+' bulges', fontsize=12)
+    fig.suptitle(guide+', '+gene_target+', '+cas_protein+', '+genome +
+                 ','+str(mm)+' mismatches'+' + '+str(bul)+' bulges', fontsize=12)
     plt.savefig(
         out_folder+f"Atitle_{guide}.pdf", transparent=True)
     plt.clf()
