aa1bdfa8d5c2fd0a1f6db8ac114fc74dc5f26be4
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-06 13:01:56 +0100

    update to plot corr with 10k and log scale

--- correlation_plot.py
+++ correlation_plot.py
@@ -174,6 +174,106 @@ def plot_correlation(original_df):
     plt.clf()
     plt.close('all')
 
+    # start figure to plot all in one plot (top10000 union with rank)
+    plt.figure()
+    # df list with all guides dfs
+    df_guide_list = list()
+
+    for guide in original_df["Spacer+PAM"].unique():
+
+        # filter the df to obtain single guide targets
+        df_guide = original_df.loc[(original_df['Spacer+PAM'] == guide)]
+        df_guide['CFD_Rank'] = np.argsort(df_guide['CFD_score_(highest_CFD)'])
+        df_guide['CRISTA_Rank'] = np.argsort(
+            df_guide['CRISTA_score_(highest_CRISTA)'])
+        df_guide['CFD_Rank'] += 1
+        df_guide['CRISTA_Rank'] += 1
+
+        df_guide_selected = df_guide.loc[(
+            df_guide['CFD_Rank'] <= 10000) | (df_guide['CRISTA_Rank'] <= 10000)]
+
+        df_guide_selected.loc[df_guide_selected['CFD_Rank']
+                              > 10000, 'CFD_Rank'] = 1000
+        df_guide_selected.loc[df_guide_selected['CRISTA_Rank']
+                              > 10000, 'CRISTA_Rank'] = 1000
+
+        df_guide_list.append(df_guide_selected)
+
+        print('count for guide', guide)
+        print('count of targets in union top10000',
+              len(df_guide_selected.index))
+        # CFD<100 & CRISTA<100
+        print('CFD<=100 & CRISTA<=100')
+        print(len(df_guide_selected[(df_guide_selected.CFD_Rank <= 100) & (
+            df_guide_selected.CRISTA_Rank <= 100)].index))
+        # CFD<100 & CRISTA>100
+        print('CFD<=100 & CRISTA>100')
+        print(len(df_guide_selected[(df_guide_selected.CFD_Rank <= 100) & (
+            df_guide_selected.CRISTA_Rank > 100)].index))
+        # CFD>100 & CRISTA<100
+        print('CFD>100 & CRISTA<=100')
+        print(len(df_guide_selected[(df_guide_selected.CFD_Rank > 100) & (
+            df_guide_selected.CRISTA_Rank <= 100)].index))
+        # CFD>100 & CRISTA>100
+        print('CFD>100 & CRISTA>100')
+        print(len(df_guide_selected[(df_guide_selected.CFD_Rank > 100) & (
+            df_guide_selected.CRISTA_Rank > 100)].index))
+
+        plot = sns.JointGrid(data=df_guide_selected, x='CFD_Rank',
+                             y='CRISTA_Rank', xlim=(10010, -10), ylim=(10010, -10), marginal_ticks=True)
+        plot.plot_joint(sns.scatterplot, alpha=0.5)
+        plot.plot_marginals(sns.histplot)
+
+        plot.ax_joint.axvline(x=100)
+        plot.ax_joint.axhline(y=100)
+        plot.set_axis_labels('CFD Rank', 'CRISTA Rank')
+
+        plt.tight_layout()
+        # single guide figure
+        plt.savefig(
+            sys.argv[2]+f'scatter_rank_CFDvCRISTA_top10000_union_{guide}.pdf')
+        plt.clf()
+        plt.close('all')
+
+    # whole figure
+    final_df = pd.concat(df_guide_list)
+
+    print('whole figure')
+    print('count of targets in union top1000',
+          len(final_df.index))
+    # CFD<100 & CRISTA<100
+    print('CFD<=100 & CRISTA<=100')
+    print(len(final_df[(final_df.CFD_Rank <= 100) & (
+        final_df.CRISTA_Rank <= 100)].index))
+    # CFD<100 & CRISTA>100
+    print('CFD<=100 & CRISTA>100')
+    print(len(final_df[(final_df.CFD_Rank <= 100) & (
+        final_df.CRISTA_Rank > 100)].index))
+    # CFD>100 & CRISTA<100
+    print('CFD>100 & CRISTA<=100')
+    print(len(final_df[(final_df.CFD_Rank > 100) & (
+        final_df.CRISTA_Rank <= 100)].index))
+    # CFD>100 & CRISTA>100
+    print('CFD>100 & CRISTA>100')
+    print(len(final_df[(final_df.CFD_Rank > 100) & (
+        final_df.CRISTA_Rank > 100)].index))
+
+    plot = sns.JointGrid(data=final_df, x='CFD_Rank',
+                         y='CRISTA_Rank', xlim=(10010, -10), ylim=(10010, -10), marginal_ticks=True)
+    plot.plot_joint(sns.scatterplot, alpha=0.5)
+    plot.plot_marginals(sns.histplot)
+
+    plot.ax_joint.axvline(x=100)
+    plot.ax_joint.axhline(y=100)
+    plot.ax_joint.set_xscale('log')
+    plot.ax_joint.set_yscale('log')
+    plot.set_axis_labels('CFD Rank', 'CRISTA Rank')
+
+    plt.tight_layout()
+    plt.savefig(sys.argv[2]+f'scatter_rank_CFDvCRISTA_top10000_union.pdf')
+    plt.clf()
+    plt.close('all')
+
 
 print('start processing')
 original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
