95e028ac6e0d4c0ef8e63f54c5d4988959c98b51
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-16 10:12:43 +0100

    update sort criteria

--- excel_writer.py
+++ excel_writer.py
@@ -2,14 +2,19 @@ import sys
 import pandas as pd
 
 print('start processing')
+# df with targets
 original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
                           na_values=['n'])
-
+# excel writer
 writer = pd.ExcelWriter(sys.argv[2]+'guide_sheets.xlsx')
+# user sort criteria
+sort_criteria = sys.argv[3]
 
 for guide in original_df['Spacer+PAM'].unique():
+    # filter df for guide
     guide_df = original_df.loc[(original_df['Spacer+PAM'] == guide)]
 
+    # select specific columns
     guide_df = guide_df[['Spacer+PAM', 'Chromosome',
                          'Start_coordinate_(highest_CFD)',
                          'Aligned_protospacer+PAM_REF_(highest_CFD)',
@@ -33,8 +38,17 @@ for guide in original_df['Spacer+PAM'].unique():
                          'Annotation_closest_gene_name',
                          'Annotation_ENCODE']]
 
-    guide_df.sort_values('CFD_score_(highest_CFD)',
-                         ascending=False, inplace=True)
+    # sort df using user criteria
+    if 'CFD' in sort_criteria:
+        guide_df.sort_values('CFD_score_(highest_CFD)',
+                             ascending=False, inplace=True)
+    elif 'CRISTA' in sort_criteria:
+        guide_df.sort_values('CRISTA_score_(highest_CRISTA)',
+                             ascending=False, inplace=True)
+    elif 'mmbul' in sort_criteria:
+        guide_df.sort_values('Mismatches+bulges_(fewest_mm+b)',
+                             ascending=True, inplace=True)
+
     # extract top 1000 rows for each guide
     guide_df = guide_df.head(1000)
 
