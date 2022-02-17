cb6e447db5ce0d58015c246c539d23bd5b3844fa
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-17 15:52:28 +0100

    update extraction to use only mm+bul cols

--- excel_writer.py
+++ excel_writer.py
@@ -5,24 +5,13 @@ print('start processing')
 # df with targets
 original_df = pd.read_csv(sys.argv[1], sep="\t", index_col=False,
                           na_values=['n'], usecols=['Spacer+PAM', 'Chromosome',
-                                                    'Start_coordinate_(highest_CFD)',
-                                                    'Aligned_protospacer+PAM_REF_(highest_CFD)',
-                                                    'Aligned_protospacer+PAM_ALT_(highest_CFD)',
-                                                    'Mismatches+bulges_(highest_CFD)',
-                                                    'CFD_score_(highest_CFD)',
-                                                    'CFD_risk_score_(highest_CFD)',
                                                     'Start_coordinate_(fewest_mm+b)',
+                                                    'Aligned_spacer+PAM_(fewest_mm+b)',
                                                     'Aligned_protospacer+PAM_REF_(fewest_mm+b)',
                                                     'Aligned_protospacer+PAM_ALT_(fewest_mm+b)',
+                                                    'Mismatches_(fewest_mm+b)',
+                                                    'Bulges_(fewest_mm+b)',
                                                     'Mismatches+bulges_(fewest_mm+b)',
-                                                    'CFD_score_(fewest_mm+b)',
-                                                    'CFD_risk_score_(highest_CFD)',
-                                                    'Start_coordinate_(highest_CRISTA)',
-                                                    'Aligned_protospacer+PAM_REF_(highest_CRISTA)',
-                                                    'Aligned_protospacer+PAM_ALT_(highest_CRISTA)',
-                                                    'Mismatches+bulges_(highest_CRISTA)',
-                                                    'CRISTA_score_(highest_CRISTA)',
-                                                    'CRISTA_risk_score_(highest_CRISTA)',
                                                     'Annotation_GENCODE',
                                                     'Annotation_closest_gene_name',
                                                     'Annotation_ENCODE'])
@@ -34,30 +23,6 @@ sort_criteria = sys.argv[3]
 for guide in original_df['Spacer+PAM'].unique():
     # filter df for guide
     guide_df = original_df.loc[(original_df['Spacer+PAM'] == guide)]
-
-    # # select specific columns
-    # guide_df = guide_df[['Spacer+PAM', 'Chromosome',
-    #                      'Start_coordinate_(highest_CFD)',
-    #                      'Aligned_protospacer+PAM_REF_(highest_CFD)',
-    #                      'Aligned_protospacer+PAM_ALT_(highest_CFD)',
-    #                      'Mismatches+bulges_(highest_CFD)',
-    #                      'CFD_score_(highest_CFD)',
-    #                      'CFD_risk_score_(highest_CFD)',
-    #                      'Start_coordinate_(fewest_mm+b)',
-    #                      'Aligned_protospacer+PAM_REF_(fewest_mm+b)',
-    #                      'Aligned_protospacer+PAM_ALT_(fewest_mm+b)',
-    #                      'Mismatches+bulges_(fewest_mm+b)',
-    #                      'CFD_score_(fewest_mm+b)',
-    #                      'CFD_risk_score_(highest_CFD)',
-    #                      'Start_coordinate_(highest_CRISTA)',
-    #                      'Aligned_protospacer+PAM_REF_(highest_CRISTA)',
-    #                      'Aligned_protospacer+PAM_ALT_(highest_CRISTA)',
-    #                      'Mismatches+bulges_(highest_CRISTA)',
-    #                      'CRISTA_score_(highest_CRISTA)',
-    #                      'CRISTA_risk_score_(highest_CRISTA)',
-    #                      'Annotation_GENCODE',
-    #                      'Annotation_closest_gene_name',
-    #                      'Annotation_ENCODE']]
 
     # sort df using user criteria
     if 'CFD' in sort_criteria:
