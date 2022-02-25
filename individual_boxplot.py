52116bd74e1fb318204d749e94b0fc9c05d2d229
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-25 10:46:31 +0100

    generate plot with ratios

--- individual_boxplot.py
+++ individual_boxplot.py
@@ -1,5 +1,16 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import warnings
+# SUPPRESS ALL WARNINGS
+warnings.filterwarnings("ignore")
+# do not use X11
+matplotlib.use('Agg')
+# set matplotlib for pdf editing
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+plt.style.use('seaborn-poster')
 
 df = pd.read_csv(sys.argv[1], sep="\t",
                  index_col=False, na_values=['n'], usecols=['Variant_samples_(highest_CFD)'])
@@ -21,7 +32,22 @@ def count_personal_and_private(row: pd.Series):
 df.apply(count_personal_and_private, axis=1)
 sample_dict.pop('NA', None)
 
+# list containing ratio for 1000G,HGDP,BOTH
+boxplot_values = [[], [], []]
 for sample in sample_dict:
     if sample_dict[sample][0] != 0:  # if personal is not zero
         # ratio=private/personal
         sample_dict[sample][2] = sample_dict[sample][1]/sample_dict[sample][0]
+        if 'HGDP' in sample:
+            boxplot_values[1].append(sample_dict[sample][2])
+        else:
+            boxplot_values[0].append(sample_dict[sample][2])
+        boxplot_values[2].append(sample_dict[sample][2])
+
+plt.figure()
+plt.boxplot(boxplot_values)
+plt.tight_layout()
+plt.savefig(
+    out_folder+f"boxplot_1000G_HGDP_both.pdf")
+plt.clf()
+plt.close('all')
