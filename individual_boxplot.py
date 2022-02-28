c64acd9a5ab9f8bad6b4d5a49776b7b60cbd9069
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-28 11:09:34 +0100

    test

--- individual_boxplot.py
+++ individual_boxplot.py
@@ -26,14 +26,14 @@ sample_dict = dict()
 
 def count_personal_and_private(sample_string: str):
     sample_list = sample_string.strip().split(',')
-    # print(sample_string)
+    print(sample_string)
     only_1000G = False
     only_HGDP = False
     if 'HGDP' not in sample_string:
         only_1000G = True
-    if len(re.findall('HG[0-9]|NA[0-9]', sample_string)) == 0:
+    if (re.search('HG[0-9]|NA[0-9]', sample_string)) is None:
         only_HGDP = True
-    # print(re.findall('HG[0-9]|NA[0-9]', sample_string))
+    print(re.search('HG[0-9]|NA[0-9]', sample_string))
     # print('1000G:', only_1000G, 'HGDP:', only_HGDP)
     for sample in sample_list:
         if sample not in sample_dict.keys():
