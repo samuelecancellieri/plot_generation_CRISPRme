dc91c03288db11352edda6d301688e183196852d
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-28 10:19:05 +0100

    update with analysis of only targets

--- individual_boxplot.py
+++ individual_boxplot.py
@@ -1,6 +1,5 @@
-from email import header
-import fileinput
 import sys
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
@@ -22,16 +21,29 @@ file_in.readline()  # skip header
 out_folder = sys.argv[2]
 sample_dict = dict()
 
+HG_PATTERN = 'HG[0-9]'
 
-def count_personal_and_private(sample_list: list):
+
+def count_personal_and_private(sample_list: str):
+    sample_string = sample_list
     sample_list = sample_list.strip().split(',')
+    only_1000G = False
+    only_HGDP = False
+    if 'HGDP' not in sample_list:
+        only_1000G = True
+    if 'NA' not in sample_list and (re.match(HG_PATTERN, sample_string) == False):
+        only_HGDP = True
     for sample in sample_list:
         if sample not in sample_dict.keys():
-            # personal,private
-            sample_dict[sample] = [0, 0]
+            # personal,private,only
+            sample_dict[sample] = [0, 0, 0]
         sample_dict[sample][0] += 1
         if len(sample_list) == 1:
             sample_dict[sample][1] += 1
+        if only_1000G:
+            sample_dict[sample][2] += 1
+        if only_HGDP:
+            sample_dict[sample][2] += 1
 
 
 for line in file_in:
@@ -42,27 +54,37 @@ for line in file_in:
 sample_dict.pop('NA', None)
 sample_dict.pop('n', None)
 # print(sample_dict)
-# list containing ratio for 1000G,HGDP,BOTH
-boxplot_values = [[], [], []]
+# list containing ratio for 1000G_shared,1000G_only,HGDP_shared,HGDP_private
+boxplot_values = [[], [], [], []]
 for sample in sample_dict:
-    ratio = 0
+    # calculate ratio with shared targets
+    ratio = 0  # shared_ratio
     if sample_dict[sample][0] != 0:  # if personal is not zero
         # ratio=private/personal
         ratio = sample_dict[sample][1]/sample_dict[sample][0]
     if 'HGDP' in sample:
-        boxplot_values[1].append(ratio)
+        boxplot_values[2].append(ratio)
     else:
         boxplot_values[0].append(ratio)
-    boxplot_values[2].append(ratio)
-# print(boxplot_values)
+    # calculate ratio with only targets
+    ratio = 0  # only_ratio
+    if sample_dict[sample][0] != 0:  # if personal is not zero
+        # ratio=private/only
+        ratio = sample_dict[sample][1]/sample_dict[sample][2]
+    if 'HGDP' in sample:
+        boxplot_values[3].append(ratio)
+    else:
+        boxplot_values[1].append(ratio)
 
-plt.figure()
-# plt.boxplot(boxplot_values)
-sns.boxplot(data=boxplot_values)
-plt.xlabel('Dataset of variant targets')
-plt.ylabel('Ratio of private targets/personal targets')
-plt.tight_layout()
-plt.savefig(
-    out_folder+f"boxplot_1000G_HGDP_both.pdf")
-plt.clf()
-plt.close('all')
+for count, elem in enumerate(boxplot_values):
+    plt.figure()
+    # plt.boxplot(boxplot_values)
+    sns.displot(elem, kind="kde")
+    # sns.boxplot(data=boxplot_values)
+    plt.xlabel('Dataset of variant targets')
+    plt.ylabel('Ratio of private targets/personal targets')
+    plt.tight_layout()
+    plt.savefig(
+        out_folder+f"{count}_boxplot_1000G_HGDP_both.pdf")
+    plt.clf()
+    plt.close('all')
