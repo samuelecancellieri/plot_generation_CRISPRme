2caf6a0adaa2550025e18beeef924a5624efbbfc
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-03-02 17:26:47 +0100

    removed print

--- individual_boxplot.py
+++ individual_boxplot.py
@@ -80,8 +80,8 @@ print('mean value single search', mean(boxplot_values_single_search[0]))
 print('mean value double search', mean(boxplot_values_double_search[0]))
 df_complete = pd.DataFrame(
     {str(analyzed_set): boxplot_values_single_search[0], 'Private': boxplot_values_single_search[1], 'Personal': boxplot_values_single_search[2], '1000G+HGDP': boxplot_values_double_search[0], 'Superpopulation': boxplot_values_single_search[3]})
-print(df_complete[str(analyzed_set)])
-print(df_complete['1000G+HGDP'])
+# print(df_complete[str(analyzed_set)])
+# print(df_complete['1000G+HGDP'])
 
 # SCATTERPLOT
 # plt.figure()
